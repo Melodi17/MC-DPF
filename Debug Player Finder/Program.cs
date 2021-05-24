92703b65753f9339d3f5d8ef4b7cd7a12204c49b
Melodi <austinfairgray@gmail.com>
2021-05-24 14:02:00 +1000

    Add Stuff

---
+++ Debug Player Finder/Program.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using ConsoleUIRenderer;
+
+namespace Debug_Player_Finder
+{
+    class Program
+    {
+        static Window mainWindow;
+        static ListBox listBox;
+        static void Main(string[] args)
+        {
+            Run();
+        }
+
+        static void Run()
+        {
+            Console.Clear();
+            Console.TreatControlCAsInput = true;
+            Console.CursorVisible = false;
+            Console.Title = "Debug Player Finder        Control+H for help";
+
+            mainWindow = new Window();
+            mainWindow.position = new Point(0, 0);
+            mainWindow.size = new Size(Console.WindowWidth, Console.WindowHeight);
+            mainWindow.foregroundColor = ConsoleColor.White;
+            mainWindow.backgroundColor = ConsoleColor.DarkBlue;
+            mainWindow.text = "Debug Player Finder";
+            mainWindow.Render();
+
+            listBox = new ListBox();
+            listBox.position = new Point(mainWindow.position.x + 1, mainWindow.position.y + 1);
+            listBox.size = new Size(mainWindow.size.width - 3, mainWindow.size.height - 3);
+            listBox.foregroundColor = mainWindow.foregroundColor;
+            listBox.backgroundColor = mainWindow.backgroundColor;
+            listBox.activeForegroundColor = mainWindow.backgroundColor;
+            listBox.activeBackgroundColor = mainWindow.foregroundColor;
+            listBox.selectedIndex = 0;
+            listBox.items.Add("Crash Minecraft");
+            listBox.items.Add("Parse Latest Debug Log");
+            listBox.items.Add("Quit");
+            listBox.Render();
+
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo consoleKeyInfo = Console.ReadKey();
+                    if (consoleKeyInfo.Modifiers == ConsoleModifiers.Control)
+                    {
+                        if (consoleKeyInfo.Key == ConsoleKey.Q)
+                        {
+                            Process.GetCurrentProcess().Kill();
+                        }
+                        if (consoleKeyInfo.Key == ConsoleKey.B)
+                        {
+                            Run();
+                        }
+                        else if (consoleKeyInfo.Key == ConsoleKey.H)
+                        {
+                            Window popupWindow = new Window();
+                            popupWindow.foregroundColor = ConsoleColor.Black;
+                            popupWindow.backgroundColor = ConsoleColor.DarkGray;
+                            popupWindow.size = new Size((mainWindow.size.width / 3) * 2, (mainWindow.size.height / 3) * 2);
+                            popupWindow.position = new Point(popupWindow.size.width / 4, popupWindow.size.height / 4);
+                            popupWindow.text = "Help Menu";
+                            popupWindow.Render();
+
+                            Label popupText = new Label();
+                            popupText.position = new Point(popupWindow.position.x + 1, popupWindow.position.y + 1);
+                            popupText.size = new Size(popupWindow.size.width - 3, popupWindow.size.height - 3);
+                            popupText.foregroundColor = popupWindow.foregroundColor;
+                            popupText.backgroundColor = popupWindow.backgroundColor;
+                            popupText.text = "----- Help -----\n";
+                            popupText.text += "This is a small command-line utility for extracting coodinates from a multiplayer server's crash logs\n";
+                            popupText.text += "\n";
+                            popupText.text += "----- Credits -----\n";
+                            popupText.text += "Made by Melodi\n";
+                            popupText.text += "Github: https://github.com/Melodi17/MC-DPF\n";
+                            popupText.text += "\n";
+                            popupText.text += "----- Shortcuts -----\n";
+                            popupText.text += "Control+H - Help\n";
+                            popupText.text += "Control+B - Restart and re-render screen\n";
+                            popupText.text += "Control+Q - Quit\n";
+                            popupText.text += "Esc       - Close menu (Only available in menus)\n";
+                            popupText.Render();
+
+                            while (true)
+                            {
+                                if (Console.KeyAvailable)
+                                {
+                                    ConsoleKeyInfo tunnelConsoleKeyInfo = Console.ReadKey();
+                                    if (tunnelConsoleKeyInfo.Key == ConsoleKey.Escape)
+                                    {
+                                        mainWindow.Render();
+                                        listBox.Render();
+                                        break;
+                                    }
+                                    if (tunnelConsoleKeyInfo.Modifiers == ConsoleModifiers.Control)
+                                    {
+                                        if (tunnelConsoleKeyInfo.Key == ConsoleKey.Q)
+                                        {
+                                            Process.GetCurrentProcess().Kill();
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (consoleKeyInfo.Key == ConsoleKey.UpArrow)
+                        {
+                            if (listBox.selectedIndex > 0)
+                            {
+                                listBox.selectedIndex--;
+                                listBox.Render();
+                            }
+                        }
+                        else if (consoleKeyInfo.Key == ConsoleKey.DownArrow)
+                        {
+                            if (listBox.selectedIndex < listBox.items.Count - 1)
+                            {
+                                listBox.selectedIndex++;
+                                listBox.Render();
+                            }
+                        }
+                        else if (consoleKeyInfo.Key == ConsoleKey.Enter)
+                        {
+                            string selectedItem = listBox.items[listBox.selectedIndex];
+                            if (selectedItem == "Quit")
+                            {
+                                Process.GetCurrentProcess().Kill();
+                            }
+                            else if (selectedItem == "Parse Latest Debug Log")
+                            {
+                                string roaming = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                                string mcdir = Path.Join(roaming, ".minecraft");
+                                string logs = Path.Join(mcdir, "logs");
+                                string debugFile = Path.Join(logs, "debug.log");
+                                if (Directory.Exists(mcdir))
+                                {
+                                    if (Directory.Exists(logs))
+                                    {
+                                        if (File.Exists(debugFile))
+                                        {
+                                            string[] file = File.ReadAllLines(debugFile);
+                                            int indexOfError = file.IndexOf("A detailed walkthrough of the error, its code path and all known details is as follows:");
+
+                                            if (indexOfError != -1)
+                                            {
+                                                int indexOfCoods = file.SoftIndexOf("	All players: ");
+                                                if (indexOfCoods != -1)
+                                                {
+                                                    string unparsedLine = file[indexOfCoods];
+                                                    try
+                                                    {
+                                                        string startParsedLine = unparsedLine.Substring(unparsedLine.IndexOf("[") + 1);
+                                                        startParsedLine = startParsedLine.Remove(startParsedLine.Length - 1);
+                                                        string[] playersAsString = startParsedLine.Split("], ");
+                                                        List<Player> players = new List<Player>();
+                                                        foreach (string item in playersAsString)
+                                                        {
+                                                            players.Add(Player.Parse(item));
+                                                        }
+
+                                                        string output = "----- Data Retrieved -----\n";
+                                                        foreach (Player item in players)
+                                                        {
+                                                            output += item.ToString() + "\n";
+                                                        }
+                                                        Popup("Results", output);
+                                                    }
+                                                    catch (Exception)
+                                                    {
+                                                        Popup("Error", "Parse Error");
+                                                    }
+                                                }
+                                                else
+                                                {
+                                                    Popup("Error", "Minecraft debug.log playerdata missing");
+                                                }
+                                            }
+                                            else
+                                            {
+                                                Popup("Error", "Minecraft debug.log exception missing");
+                                            }
+                                        }
+                                        else
+                                        {
+                                            Popup("Error", "Minecraft debug.log file missing");
+                                        }
+                                    }
+                                    else
+                                    {
+                                        Popup("Error", "Minecraft log folder missing");
+                                    }
+                                }
+                                else
+                                {
+                                    Popup("Error", "Minecraft not installed");
+                                }
+                            }
+                            else if (selectedItem == "Crash Minecraft")
+                            {
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        static void Popup(string title, string content)
+        {
+            Window popupWindow = new Window();
+            popupWindow.foregroundColor = ConsoleColor.Black;
+            popupWindow.backgroundColor = ConsoleColor.DarkGray;
+            popupWindow.size = new Size((mainWindow.size.width / 3) * 2, (mainWindow.size.height / 3) * 2);
+            popupWindow.position = new Point(popupWindow.size.width / 4, popupWindow.size.height / 4);
+            popupWindow.text = title;
+            popupWindow.Render();
+
+            Label popupText = new Label();
+            popupText.position = new Point(popupWindow.position.x + 1, popupWindow.position.y + 1);
+            popupText.size = new Size(popupWindow.size.width - 3, popupWindow.size.height - 3);
+            popupText.foregroundColor = popupWindow.foregroundColor;
+            popupText.backgroundColor = popupWindow.backgroundColor;
+            popupText.text = content;
+            popupText.Render();
+
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo tunnelConsoleKeyInfo = Console.ReadKey();
+                    if (tunnelConsoleKeyInfo.Key == ConsoleKey.Escape)
+                    {
+                        mainWindow.Render();
+                        listBox.Render();
+                        break;
+                    }
+                    if (tunnelConsoleKeyInfo.Modifiers == ConsoleModifiers.Control)
+                    {
+                        if (tunnelConsoleKeyInfo.Key == ConsoleKey.Q)
+                        {
+                            Process.GetCurrentProcess().Kill();
+                        }
+                    }
+                }
+            }
+        }
+    }
+    static class EXT
+    {
+        public static int IndexOf(this string[] arr, string match)
+        {
+            int i = 0;
+            foreach (string item in arr)
+            {
+                if (item == match)
+                {
+                    return i;
+                }
+                i++;
+            }
+            return -1;
+        }
+        public static int SoftIndexOf(this string[] arr, string match)
+        {
+            int i = 0;
+            foreach (string item in arr)
+            {
+                if (item.StartsWith(match))
+                {
+                    return i;
+                }
+                i++;
+            }
+            return -1;
+        }
+        public static string SkipString(this string str, string match)
+        {
+            if (str.StartsWith(match) && match != "")
+            {
+                return str.Substring(match.Length - 1);
+            }
+            else
+            {
+                return str;
+            }
+        }
+    }
+    class Player
+    {
+        public string name;
+        public double x;
+        public double y;
+        public double z;
+        public static Player Parse(string str)
+        {
+            Player player = new Player();
+
+            string leftover = str.Replace("RemoteClientPlayerEntity['", "").Replace("ClientPlayerEntity['", "");
+            if (leftover[^1] == ']')
+            {
+                leftover = leftover.Remove(leftover.Length - 1);
+            }
+            player.name = leftover.Substring(0, leftover.IndexOf("'"));
+            leftover = leftover.SkipString(player.name + "'");
+            player.x = Convert.ToDouble(leftover.Split(", ")[2].Split("=")[1]);
+            player.y = Convert.ToDouble(leftover.Split(", ")[3].Split("=")[1]);
+            player.z = Convert.ToDouble(leftover.Split(", ")[4].Split("=")[1]);
+
+            return player;
+        }
+        public override string ToString()
+        {
+            return $"name:{name} x:{x} y:{y} z:{z}";
+        }
+    }
+}
---
+++ Debug Player Finder/UIRenderer.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUIRenderer
+{
+    public class Control
+    {
+        public Point position;
+        public ConsoleColor backgroundColor;
+        public ConsoleColor foregroundColor;
+
+        public void Render()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    #region Controls
+    public class Window : Control
+    {
+        public Size size;
+        public string text = "Window";
+        public new void Render()
+        {
+            Point _curpos = Renderer.GetCursorPosition();
+
+            Renderer.SetCursorPosition(this.position);
+            Console.ForegroundColor = this.foregroundColor;
+            Console.BackgroundColor = this.backgroundColor;
+
+            if (size.height > 5 && size.width > this.text.Length + 5)
+            {
+                string topLine = "┌ " + this.text + " " + new string('─', Math.Max(1, size.width - (5 + this.text.Length))) + "┐";
+                Console.WriteLine(topLine);
+
+                for (int i = 0; i < size.height - 1; i++)
+                {
+                    Point curpos = Renderer.GetCursorPosition();
+                    Renderer.SetCursorPosition(new Point(position.x, curpos.y));
+                    if (i == size.height - 2)
+                    {
+                        Console.WriteLine("└" + new string('─', Math.Max(1, size.width - 3)) + "┘");
+                    }
+                    else
+                    {
+                        Console.WriteLine("│" + new string(' ', Math.Max(1, size.width - 3)) + "│");
+                    }
+                }
+            }
+            else
+            {
+
+            }
+
+            Console.ResetColor();
+            Renderer.SetCursorPosition(_curpos);
+        }
+    }
+    public class Label : Control
+    {
+        public Size size;
+        public string text = "Label";
+        public new void Render()
+        {
+            string wrappedText = WordWrap(text, size.width).Replace("\r", "");
+
+            Point _curpos = Renderer.GetCursorPosition();
+
+            Renderer.SetCursorPosition(this.position);
+            Console.ForegroundColor = this.foregroundColor;
+            Console.BackgroundColor = this.backgroundColor;
+            int i = 0;
+            foreach (string item in wrappedText.Split("\n"))
+            {
+                if (i < size.height)
+                {
+                    Point curpos = Renderer.GetCursorPosition();
+                    Renderer.SetCursorPosition(new Point(position.x, curpos.y));
+                    Console.WriteLine(item + new string(' ', Math.Max(0, this.size.width - (item.Length + 1))));
+                }
+                else
+                {
+                    break;
+                }
+                i++;
+            }
+            Console.ResetColor();
+            Renderer.SetCursorPosition(_curpos);
+        }
+        /// <summary>
+        /// Word wraps the given text to fit within the specified width.
+        /// </summary>
+        /// <param name="text">Text to be word wrapped</param>
+        /// <param name="width">Width, in characters, to which the text
+        /// should be word wrapped</param>
+        /// <returns>The modified text</returns>
+        public static string WordWrap(string text, int width)
+        {
+            int pos, next;
+            StringBuilder sb = new StringBuilder();
+
+            // Lucidity check
+            if (width < 1)
+                return text;
+
+            // Parse each line of text
+            for (pos = 0; pos < text.Length; pos = next)
+            {
+                // Find end of line
+                int eol = text.IndexOf("\n", pos);
+                if (eol == -1)
+                    next = eol = text.Length;
+                else
+                    next = eol + "\n".Length;
+
+                // Copy this line of text, breaking into smaller lines as needed
+                if (eol > pos)
+                {
+                    do
+                    {
+                        int len = eol - pos;
+                        if (len > width)
+                            len = BreakLine(text, pos, width);
+                        sb.Append(text, pos, len);
+                        sb.Append("\n");
+
+                        // Trim whitespace following break
+                        pos += len;
+                        while (pos < eol && Char.IsWhiteSpace(text[pos]))
+                            pos++;
+                    } while (eol > pos);
+                }
+                else sb.Append("\n"); // Empty line
+            }
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Locates position to break the given line so as to avoid
+        /// breaking words.
+        /// </summary>
+        /// <param name="text">String that contains line of text</param>
+        /// <param name="pos">Index where line of text starts</param>
+        /// <param name="max">Maximum line length</param>
+        /// <returns>The modified line length</returns>
+        private static int BreakLine(string text, int pos, int max)
+        {
+            // Find last whitespace in line
+            int i = max;
+            while (i >= 0 && !Char.IsWhiteSpace(text[pos + i]))
+                i--;
+
+            // If no whitespace found, break at maximum length
+            if (i < 0)
+                return max;
+
+            // Find start of whitespace
+            while (i >= 0 && Char.IsWhiteSpace(text[pos + i]))
+                i--;
+
+            // Return length of text before whitespace
+            return i + 1;
+        }
+    }
+    public class Checkmark : Control
+    {
+        public string text = "Checkmark";
+        public bool active = false;
+        public bool toggled = false;
+        public ConsoleColor activeBackgroundColor;
+        public ConsoleColor activeForegroundColor;
+        public new void Render()
+        {
+            Point _curpos = Renderer.GetCursorPosition();
+
+            Renderer.SetCursorPosition(this.position);
+            if (active)
+            {
+                Console.ForegroundColor = this.activeForegroundColor;
+                Console.BackgroundColor = this.activeBackgroundColor;
+            }
+            else
+            {
+                Console.ForegroundColor = this.foregroundColor;
+                Console.BackgroundColor = this.backgroundColor;
+            }
+
+            if (toggled)
+            {
+                Console.WriteLine("✓ " + text);
+            }
+            else
+            {
+                Console.WriteLine("- " + text);
+            }
+
+            Console.ResetColor();
+            Renderer.SetCursorPosition(_curpos);
+        }
+    }
+    public class Button : Control
+    {
+        public string text = "Button";
+        public bool active = false;
+        public ConsoleColor activeBackgroundColor;
+        public ConsoleColor activeForegroundColor;
+        public new void Render()
+        {
+            Point _curpos = Renderer.GetCursorPosition();
+
+
+            Renderer.SetCursorPosition(this.position);
+            if (active)
+            {
+                Console.ForegroundColor = this.activeForegroundColor;
+                Console.BackgroundColor = this.activeBackgroundColor;
+            }
+            else
+            {
+                Console.ForegroundColor = this.foregroundColor;
+                Console.BackgroundColor = this.backgroundColor;
+            }
+
+            Console.WriteLine("[ " + text + " ]");
+
+            Console.ResetColor();
+            Renderer.SetCursorPosition(_curpos);
+        }
+    }
+    public class ListBox : Control
+    {
+        public Size size;
+        public int selectedIndex;
+        public ConsoleColor activeBackgroundColor;
+        public ConsoleColor activeForegroundColor;
+        public List<string> items = new List<string>();
+        public new void Render()
+        {
+            Point _curpos = Renderer.GetCursorPosition();
+
+            Renderer.SetCursorPosition(this.position);
+            Console.ForegroundColor = this.foregroundColor;
+            Console.BackgroundColor = this.backgroundColor;
+
+            int i = 0;
+            foreach (string item in items)
+            {
+                Point curpos = Renderer.GetCursorPosition();
+                Renderer.SetCursorPosition(new Point(position.x, curpos.y));
+                if (i == selectedIndex)
+                {
+                    Console.ForegroundColor = this.activeForegroundColor;
+                    Console.BackgroundColor = this.activeBackgroundColor;
+                    Console.WriteLine(item + new string(' ', Math.Max(0, this.size.width - item.Length)));
+                    Console.ForegroundColor = this.foregroundColor;
+                    Console.BackgroundColor = this.backgroundColor;
+                }
+                else
+                {
+                    Console.WriteLine(item + new string(' ', Math.Max(0, this.size.width - item.Length)));
+                }
+                i++;
+            }
+            for (int x = 0; x < this.size.height - this.items.Count; x++)
+            {
+                Point curpos = Renderer.GetCursorPosition();
+                Renderer.SetCursorPosition(new Point(position.x, curpos.y));
+                Console.WriteLine(new string(' ', Math.Max(0, this.size.width)));
+            }
+
+            Console.ResetColor();
+            Renderer.SetCursorPosition(_curpos);
+        }
+    }
+    public class ProgressBar : Control
+    {
+
+        public Size size;
+        public ConsoleColor activeBackgroundColor;
+        public ConsoleColor activeForegroundColor;
+        public int value;
+        public new void Render()
+        {
+            Point _curpos = Renderer.GetCursorPosition();
+
+            Renderer.SetCursorPosition(this.position);
+            Console.ForegroundColor = this.activeForegroundColor;
+            Console.BackgroundColor = this.activeBackgroundColor;
+
+            Console.Write(new string(' ', Math.Max(0, Math.Min(this.size.width, value))));
+
+            Console.ForegroundColor = this.foregroundColor;
+            Console.BackgroundColor = this.backgroundColor;
+
+            Console.WriteLine(new string(' ', Math.Max(0, this.size.width - value)));
+
+            Console.ResetColor();
+            Renderer.SetCursorPosition(_curpos);
+        }
+    }
+    public class TextBox : Control
+    {
+        public string text;
+
+    }
+    #endregion
+    public class Point
+    {
+        public int x;
+        public int y;
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    public class Size
+    {
+        public int width;
+        public int height;
+        public Size(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+    }
+    public class Renderer
+    {
+        public static void SetCursorPosition(Point point)
+        {
+            Console.SetCursorPosition(point.x, point.y);
+        }
+        public static Point GetCursorPosition()
+        {
+            return new Point(Console.CursorLeft, Console.CursorTop);
+        }
+        public static void SetText(string text)
+        {
+
+        }
+    }
+}
